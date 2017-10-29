571b0c1d6f9410b0a97e38ccdd1e10289cc6c403
Gorilla-Zhang <841845663@qq.com>
2017-10-30 01:08:56 +0800

    zxt

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
 export default class ComponentHeader extends React.Component{
   render(){
     return (
       <header>
-        <h1>这里是头部</h1>
+        <h1>这里是头部</h1>  
       </header>  
     )
   }
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,25 @@
 import ComponentHeader from './components/header'
+import ComponentFooter from './components/footer'
+import BodyIndex from './components/BodyIndex'
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-export default class Index extends React.Component{
-  render(){
-    return(
-      <ComponentHeader/>
+export default class Index extends React.Component {
+  render() {
+    // var component
+    // if(用户已登录){
+    //   component = <ComponentLoginHeader/>
+    // }
+    // else{
+    //   component = <ComponentFooter/>
+    // }
+
+    return (
+      <div>
+        <ComponentHeader/>
+        <BodyIndex/>
+        <ComponentFooter/>
+      </div>
     )
   }
 }
