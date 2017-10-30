d4399cbb6e939f43df8734af769435a6ec22fd7e
Gorilla-Zhang <841845663@qq.com>
2017-10-30 20:21:54 +0800

    zxt

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -2,19 +2,33 @@ import React from 'react'
 
 export default class ComponentHeader extends React.Component {
 
+  constructor(){
+    super()
+    this.state={
+      miniHeader:false //默认加载时不是mini的头部
+    }
+  }
+  switchHeader(){
+    this.setState({
+      miniHeader:!this.state.miniHeader
+    })
+  }
+
+
   render() {
     const styleComponentHeader = {
       header: {
         backgroundColor: "#333333",
         color: "#FFFFFF",
-        paddingTop:"15px",
-        paddingBottom: "15px"
+        paddingTop:(this.state.miniHeader)?"3px":"15px",
+        paddingBottom:(this.state.miniHeader)?"3px":"15px"
       }
       //还可以定义其他样式
     }
 
     return (
-      <header style={styleComponentHeader.header} className="smallFontSize">
+      <header style={styleComponentHeader.header} className="smallFontSize"
+      onClick={this.switchHeader.bind(this)}>
         <h1>这里是头部</h1>
       </header>
     )
