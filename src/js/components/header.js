4bde418c179edfbcc3a9cfe932e1bdac7f48f7ca
Gorilla-Zhang <841845663@qq.com>
2017-10-30 19:19:37 +0800

    zxt

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
-export default class ComponentHeader extends React.Component{
-  render(){
+export default class ComponentHeader extends React.Component {
+
+  render() {
+    const styleComponentHeader = {
+      header: {
+        backgroundColor: "#333333",
+        color: "#FFFFFF",
+        paddingTop:"15px",
+        paddingBottom: "15px"
+      }
+      //还可以定义其他样式
+    }
+
     return (
-      <header>
-        <h1>这里是头部</h1>  
-      </header>  
+      <header style={styleComponentHeader.header} className="smallFontSize">
+        <h1>这里是头部</h1>
+      </header>
     )
   }
 }
