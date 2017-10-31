9e671109f8f009857333b7ff7e12023d759eebb9
Gorilla-Zhang <841845663@qq.com>
2017-10-31 12:40:56 +0800

    zxt

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -31,9 +31,9 @@ export default class ComponentHeader extends React.Component {
       <header style={styleComponentHeader.header} className="smallFontSize">
         <h1>这里是头部</h1>
         <ul>
-          <li><link to={'/'}/>首页</li>
-          <li><link to={'/list'}/>列表页面</li>
-          <li><link to={'/details'}/>被嵌套的详细页面</li>
+          <li><Link to={'/'}/>首页</li>
+          <li><Link to={'/list'}/>列表页面</li>
+          <li><Link to={'/details'}/>被嵌套的详细页面</li>
         </ul>
       </header>
     )
