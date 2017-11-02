1ecc08c494d373d9cc22870943bd52094ca20c64
Gorilla-Zhang <841845663@qq.com>
2017-11-02 14:51:37 +0800

    zxt

--- src/js/components/mixins.js
+++
@@ -1,9 +0,0 @@
-const MixinLog = {
-  componentDidMount(){
-    console.log("MixinLog componentDidMount")
-  },
-  log(){
-    console.log("abcd...")
-  }
-}
-export default MixinLog
--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,12 @@
-import ComponentHeader from './components/header'
-import ComponentFooter from './components/footer'
-import BodyIndex from './components/BodyIndex'
-import 'antd/dist/antd.css'
-var React = require('react')
-var ReactDOM = require('react-dom')
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-export default class Index extends React.Component {
-
-  componentWillMount() {
-    //定义你的逻辑即可
-    console.log("Index - componentWillMount");
-  }
-  componentDidMount() {
-    console.log("Index - componentDidMount");
-  }
-
-  render() {
-    // var component
-    // if(用户已登录){
-    //   component = <ComponentLoginHeader/>
-    // }
-    // else{
-    //   component = <ComponentFooter/>
-    // }
-
-    return (
-      <div>
-        <ComponentHeader />
-        <BodyIndex userid={123456} username={"parry"} />
-        <div>
-          {this.props.children}
-        </div>
-        <ComponentFooter />
-      </div>
+export default class Index extends React.Component{
+  render(){
+    return(
+      <div>Init</div>
     )
   }
 }
+
+// ReactDOM.render(<Index/>,document.getElementById(''))
