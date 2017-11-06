3f1314708652ebb64e0a5adcd44422f7b3508896
Gorilla-Zhang <841845663@qq.com>
2017-11-06 11:05:42 +0800

    zxt

--- src/js/components/pc_footer.js
+++ src/js/components/pc_footer.js
@@ -4,10 +4,10 @@ import { Row, Col } from 'antd'
 export default class PCFooter extends React.Component{
 render(){
   return(
-    <footer>
+    <footer style={{color:'red'}}>
        <Row>
          <Col span={2}></Col>
-         <Col span={4} style={{color:'red'}}>
+         <Col span={4}>
          &copy;&nbsp;2016 ReactNews. All Rights Reserved.
          </Col>
          <Col span={2}></Col>
--- src/js/components/pc_newscontainer.js
+++ src/js/components/pc_newscontainer.js
@@ -17,13 +17,13 @@ export default class PCNewsContainer extends React.Component{
       <Row>
           <Col span={2}></Col>
           <Col span={20} className="containe">
-          <div className="leftContainer">
-            <div className="carousel">
+          <div className="leftContainer" style={{width:400,float:'left' }}>
+            <div className="carousel" style={{width:400,float:'left'}}>
               <Carousel {...settings}>
-              <div><img src=""/></div>
-              <div><img src=""/></div>
-              <div><img src=""/></div>
-              <div><img src=""/></div>
+              <div><img src="../../images/1.png"/></div>
+              <div><img src="../../images/2.png"/></div>
+              <div><img src="../../images/3.png"/></div>
+              <div><img src="../../images/4.png"/></div>
               </Carousel>
             </div>
           </div>
