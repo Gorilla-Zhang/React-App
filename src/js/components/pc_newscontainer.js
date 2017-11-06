209989136f5a3f113edb50aeccf1a971018a0d10
Gorilla-Zhang <841845663@qq.com>
2017-11-06 11:26:39 +0800

    zxt

--- src/js/components/pc_newscontainer.js
+++ src/js/components/pc_newscontainer.js
@@ -1,37 +1,37 @@
 import React from 'react'
 import { Row, Col } from 'antd'
-import {Tabs,Carousel} from 'antd'
+import { Tabs, Carousel } from 'antd'
 const TabPane = Tabs.TabPane
-export default class PCNewsContainer extends React.Component{
- render(){
-   const settings = {
-     dots:true,
-     infinite:true,
-     speed:500,
-     slideToShow:1,
-     sutoplay:true
-   }
+export default class PCNewsContainer extends React.Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slideToShow: 1,
+      autoplay: true
+    }
 
-  return(
-    <div>
-      <Row>
+    return (
+      <div>
+        <Row>
           <Col span={2}></Col>
-          <Col span={20} className="containe">
-          <div className="leftContainer" style={{width:400,float:'left'}}>
-            <div className="carousel" style={{width:400,float:'left'}}>
-              <Carousel {...settings}>
-              <div><img src="../../images/1.png"/></div>
-              <div><img src="../../images/2.png"/></div>
-              <div><img src="../../images/3.png"/></div>
-              <div><img src="../../images/4.png"/></div>
-              </Carousel>
+          <Col span={20} className="container" style={{ paddingTop: 10 }}>
+            <div className="leftContainer" style={{ width: 400, height: 300, float: 'left' }}>
+              <div className="carousel" style={{ width: 400, height: 300, float: 'left' }}>
+                <Carousel {...settings}>
+                  <div><img src={require("../../images/1.png")} /></div>
+                  <div><img src={require("../../images/2.png")} /></div>
+                  <div><img src={require("../../images/3.png")} /></div>
+                  <div><img src={require("../../images/4.png")} /></div>
+                </Carousel>
+              </div>
             </div>
-          </div>
           </Col>
           <Col span={2}></Col>
         </Row>
-    </div>
-  )
- }
+      </div>
+    )
+  }
 
 }
